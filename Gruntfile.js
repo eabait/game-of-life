1df88e717896329abfb177d3b0a9c11e2cc28cbc
eabait <estebanabait@gmail.com>
2014-09-30 16:51:05 -0300

    basic build system in Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,41 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+
+    jshint: {
+      all: ['Gruntfile.js', 'src/**/*.js']
+    },
+
+    jasmine: {
+      pivotal: {
+        src: 'src/**/*.js',
+        options: {
+          specs: 'spec/*-spec.js',
+          keepRunner: true
+        }
+      }
+    },
+
+    complexity: {
+      generic: {
+        src: ['src/**/*.js'],
+        options: {
+          breakOnErrors: true,
+          errorsOnly: false,               // show only maintainability errors
+          cyclomatic: [3, 7, 12],          // or optionally a single value, like 3
+          halstead: 10,           // or optionally a single value, like 8
+          maintainability: 100,
+          hideComplexFunctions: false,     // only display maintainability
+          broadcast: false                 // broadcast data over event-bus
+        }
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-complexity');
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  grunt.registerTask('default', ['jshint', 'complexity', 'jasmine']);
+};

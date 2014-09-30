6e487f8d06028c20ed74b8f7c1f1eff6b4ca2f94
eabait <estebanabait@gmail.com>
2014-09-30 15:42:52 -0300

    Added comments to cell definition

--- src/cell.js
+++ src/cell.js
@@ -1,11 +1,19 @@
 var Cell = (function() {
   'use strict';
 
+  /**
+   * Cell function constructor
+   * @param {boolean} alive     Initial cell state. If true is alive, dead otherwise
+   * @param {Array} neighbours  Cell's neighbours
+   */
   function Cell(alive, neighbours) {
     this.alive = alive;
     this.neighbours = neighbours;
   }
 
+  /**
+   * @return {boolean} Returns true if this cell will live to the next generation
+   */
   Cell.prototype.willLive = function() {
     var aliveNeighbours = this.getNumberOfAliveNeighbours();
 
@@ -15,28 +23,51 @@ var Cell = (function() {
            this.shouldLiveByReproduction(aliveNeighbours));
   };
 
+  /**
+   * @param  {Number} aliveNeighbours number of alive neighbours
+   * @return {boolean}  true if the cell should die by under-population
+   */
   Cell.prototype.shouldDieByUnderPopulation = function(aliveNeighbours) {
     return (this.isAlive() && aliveNeighbours < 2);
   };
 
+  /**
+   * @param  {Number} aliveNeighbours number of alive neighbours
+   * @return {boolean}  true if the cell should live to next generation
+   */
   Cell.prototype.shouldLiveToNextGeneration = function(aliveNeighbours) {
     return (this.isAlive() && (aliveNeighbours >= 2 || aliveNeighbours <= 3));
   };
 
+  /**
+   * @param  {Number} aliveNeighbours number of alive neighbours
+   * @return {boolean}  true if the cell should die by over-population
+   */
   Cell.prototype.shouldDieByOverPopulation = function(aliveNeighbours) {
     return (this.isAlive() && aliveNeighbours > 3);
   };
 
+  /**
+   * @param  {Number} aliveNeighbours number of alive neighbours
+   * @return {boolean}  true if the cell should live by reproduction
+   */
   Cell.prototype.shouldLiveByReproduction = function(aliveNeighbours) {
     return (!this.isAlive() && aliveNeighbours === 3);
   };
 
+  /**
+   * Returns the number of cell's living neighbours
+   * @return {Number} number of this cell neighbours that are alive
+   */
   Cell.prototype.getNumberOfAliveNeighbours = function() {
     return this.neighbours.reduce(function(aliveNeighbours, neighbour) {
       return aliveNeighbours + (+neighbour.isAlive());
     }, 0);
   };
 
+  /**
+   * @return {Boolean} Returns true if the cell is alive, false otherwise
+   */
   Cell.prototype.isAlive = function() {
     return this.alive;
   };

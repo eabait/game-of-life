4151b3dbfd83427abf0b990cb9602bd49d1cee6b
eabait <estebanabait@gmail.com>
2014-10-07 08:49:37 -0300

    WIP universe spec with custom matcher

--- spec/universe-spec.js
+++ spec/universe-spec.js
@@ -15,12 +15,45 @@ describe('Universe', function() {
       [false, true, true, false],
       [false, false, false, false]
     ];
+
+    jasmine.addMatchers({
+      toBeExponentiallyLessThan: function() {
+        return {
+          compare: function(actual, expected, scaleFactor) {
+            var compareUniverses = function() {
+              actual.reduce(function() {
+
+              }, true);
+            };
+            return {
+              pass: actual < (expected / (10 || scaleFactor))
+            };
+          }
+        };
+      }
+    });
   });
 
   it('should be able to initialize', function() {
     var universe = new Universe(seedUniverse);
     expect(universe.currentGeneration.length).toBe(seedUniverse.length);
     expect(universe.getCellAt(0, 0)).toBeDefined();
+  });
+
+  describe('Calculating generations', function() {
+
+    it('should be able to calculate univere for block configuration', function() {
+      seedUniverse = [
+        [false, false, false, false],
+        [false, true, true, false],
+        [false, true, true, false],
+        [false, false, false, false]
+      ];
+
+
+
+    });
+
   });
 
 });
